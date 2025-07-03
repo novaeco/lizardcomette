5ae72c776f97adbdb19361f7e12b41ef5b8d4ad5
novaeco <novatribolnovea@gmail.com>
2025-07-03 13:52:59 +0200

    Add hardware drivers and bus configuration

--- reptile_manager/src/hardware/actuators.rs
+++ reptile_manager/src/hardware/actuators.rs
@@ -1,8 +1,22 @@
 //! Actionneurs contrôlés par le système.
 
-/// Active un actionneur spécifique.
+use ds18b20::Ds18b20;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::prelude::*;
+use onewire::OneWire;
+
+/// Initialise le bus OneWire et active un actionneur.
 pub fn activer() {
-    // TODO: activer l'actionneur
+    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
+
+    let pin = PinDriver::input_output(peripherals.pins.gpio4).unwrap();
+    let mut one = OneWire::new(pin).unwrap();
+
+    // Exemple : recherche d'un périphérique OneWire, ici un capteur DS18B20
+    if let Ok(Some(device)) = one.reset_search().next() {
+        let mut sensor = Ds18b20::new(device).unwrap();
+        let _ = sensor.start_temp_measurement(&mut one);
+    }
 }
 
 #[cfg(test)]
