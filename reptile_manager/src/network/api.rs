4586ae4d17a95c1f9cdc5e3188fe2d4b30ad8fad
novaeco <novatribolnovea@gmail.com>
2025-07-03 14:18:07 +0200

    Add Wi-Fi event handling, REST API routes and MQTT

--- reptile_manager/src/network/api.rs
+++ reptile_manager/src/network/api.rs
@@ -1,26 +1,51 @@
 //! Serveur HTTP exposant les routes de l'application et intégrant MQTT.
 
+use embedded_svc::http::Method;
+use embedded_svc::mqtt::client::{Client, QoS};
+use embedded_svc::io::Write;
+use esp_idf_svc::http::server::{EspHttpServer, Configuration as HttpConfig};
+use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration};
+use log::info;
+
 /// Gère l'API haut niveau.
 pub struct ApiServer {
-    // TODO: stocker les gestionnaires HTTP/MQTT
+    http: EspHttpServer<'static>,
+    mqtt: EspMqttClient,
 }
 
 impl ApiServer {
     /// Crée une nouvelle instance d'[`ApiServer`].
-    pub fn new() -> Self {
-        Self {
-            // TODO: initialiser les gestionnaires
-        }
+    pub fn new() -> anyhow::Result<Self> {
+        let http = EspHttpServer::new(&HttpConfig::default())?;
+        let (mqtt, _connection) = EspMqttClient::new(
+            "mqtt://broker.hivemq.com",
+            &MqttClientConfiguration::default(),
+        )?;
+
+        Ok(Self { http, mqtt })
     }
 
     /// Lance le serveur HTTP et définit les routes.
-    pub fn start(&mut self) {
-        // TODO: définir les routes HTTP
+    pub fn start(&mut self) -> anyhow::Result<()> {
+        self.http.fn_handler("/status", Method::Get, |req| {
+            info!("GET /status");
+            req.into_ok_response()?.write_all(b"ok")?;
+            Ok(())
+        })?;
+
+        self.http.fn_handler("/reptiles", Method::Get, |req| {
+            info!("GET /reptiles");
+            req.into_ok_response()?.write_all(b"[]")?;
+            Ok(())
+        })?;
+
+        Ok(())
     }
 
     /// Publie un message via MQTT.
-    pub fn publish_status(&self) {
-        // TODO: publier l'état via MQTT
+    pub fn publish_status(&self) -> anyhow::Result<()> {
+        self.mqtt.publish("reptile/status", QoS::AtLeastOnce, false, b"ok")?;
+        Ok(())
     }
 }
 
--- reptile_manager/src/network/wifi.rs
+++ reptile_manager/src/network/wifi.rs
@@ -1,31 +1,54 @@
 //! Gestion du module Wi-Fi.
 
-/// Structure simplifiée gérant la connexion Wi-Fi.
-pub struct WifiManager {
-    // TODO: stocker le pilote Wi-Fi ou les paramètres nécessaires
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::eventloop::{EspEventLoop, EspSubscription, System};
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::wifi::{EspWifi, WifiEvent};
+use log::{info, warn};
+
+/// Structure simplifiée gérant la connexion Wi-Fi et les événements.
+pub struct WifiManager<'a> {
+    wifi: EspWifi<'a>,
+    _subscription: EspSubscription<'a, System>,
 }
 
 impl WifiManager {
     /// Crée un nouveau [`WifiManager`].
-    pub fn new() -> Self {
-        Self {
-            // TODO: initialiser les champs
-        }
+    pub fn new(peripherals: Peripherals) -> anyhow::Result<Self> {
+        let sysloop = EspEventLoop::take()?;
+        let nvs = EspDefaultNvsPartition::take()?;
+        let mut wifi = EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?;
+
+        let subscription = sysloop.subscribe::<WifiEvent, _>(|event| match event {
+            WifiEvent::StaConnected => info!("Wi-Fi connecté"),
+            WifiEvent::StaDisconnected => warn!("Wi-Fi déconnecté"),
+            _ => {}
+        })?;
+
+        Ok(Self { wifi, _subscription: subscription })
     }
 
     /// Connecte l'appareil au réseau.
-    pub fn connect(&mut self) {
-        // TODO: implémenter la connexion Wi-Fi
+    pub fn connect(&mut self, ssid: &str, password: &str) -> anyhow::Result<()> {
+        self.wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: ssid.into(),
+            password: password.into(),
+            ..Default::default()
+        }))?;
+        self.wifi.start()?;
+        self.wifi.connect()?;
+        Ok(())
     }
 
     /// Callback lorsque la connexion est établie.
     pub fn on_connected(&mut self) {
-        // TODO: réagir à la connexion
+        info!("Connexion Wi-Fi établie");
     }
 
     /// Callback lorsque la connexion est perdue.
     pub fn on_disconnected(&mut self) {
-        // TODO: réagir à la déconnexion
+        warn!("Connexion Wi-Fi perdue");
     }
 }
 
